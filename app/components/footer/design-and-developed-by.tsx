18d2730276fd9625d440a246d310940698299857
Daniel Mekonnen <100097646+DanielKinnito@users.noreply.github.com>
2025-01-09 05:15:31 -0800

    Update design-and-developed-by.tsx

--- app/components/footer/design-and-developed-by.tsx
+++ app/components/footer/design-and-developed-by.tsx
@@ -1,15 +1,14 @@
 import { GITHUB_LINK } from "@/lib/constants";
-const LINK = '<https://github.com/programmer-rahul>';
 
 
 export default function DesignedAndDevelopedBy() {
   return (
     <div className="text-center text-xs md:text-lg">
       <p>
-        &#169; Designed And Developed By 💚{" "}
+        &#169; Designed And Developed By {" "}
         <b className="transition hover:cursor-pointer hover:text-main hover:underline">
-          <a href={LINK} target="_blank">
-            Rahul
+          <a href={GITHUB_LINK} target="_blank">
+            DanielKinnito
           </a>
         </b>
       </p>
