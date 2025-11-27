6336b43a135e20a09d5ca8269d08e7ba31120e3d
DanielKinnito <mokonnendaniel@gmail.com>
2025-11-27 12:05:59 +0300

    :fire: :bug: feat: add constant links for contact and social profiles

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 const GMAIL_LINK = "mokonnendaniel@gmail.com";
 const GITHUB_LINK = "https://github.com/DanielKinnito";
-const INSTAGRAM_LINK = "https://www.instagram.com/dani_mek_116";
 const LINKEDIN_LINK = "https://www.linkedin.com/in/mekonnen-daniel";
-const RESUME_LINK = "https://drive.google.com/file/d/1k0kGAOar1-FXR0yBDt9uBbRUsE9qGrD3/view?usp=drive_link";
+const RESUME_LINK = "https://drive.google.com/file/d/1EJ89ZRNQ2IJ_k2e2jLre0KkFp3pXFEFf/view?usp=sharing";
 
-export { GMAIL_LINK, GITHUB_LINK, INSTAGRAM_LINK, LINKEDIN_LINK, RESUME_LINK };
+export { GMAIL_LINK, GITHUB_LINK, LINKEDIN_LINK, RESUME_LINK };
